4500597a85bcafd32837f6a5efafb070c93ffa7c
Ivan Carlos <ivanicob@gmail.com>
2021-09-12 21:13:12 -0300

    Add Users List at home

--- module-web/src/app/home/home.component.ts
+++ module-web/src/app/home/home.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SubSink } from 'subsink';
+import { User } from '../models/User';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -7,17 +10,39 @@ import { UserService } from '../services/user.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private subscriptions = new SubSink();
+  public users: any;
 
   constructor(private router: Router, private userService: UserService) { 
   }
 
   ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  public loadUsers(): void{
+
+    this.subscriptions.add(
+      this.userService.findAll()
+      .subscribe(
+        (result: any) => {
+          this.users = Object.values(JSON.parse(result)['data']);
+      }, (error: HttpErrorResponse) => {
+        alert(error.message);
+        //this.errorMessage = error.message;
+      })
+    );
   }
 
   onLogout() {
     this.userService.logOut();
     this.router.navigateByUrl('/login');
   }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }   
 
 }
