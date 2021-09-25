9b1cf595de75d16c76942b06df073c268568e5b6
Ivan Carlos <ivanicob@gmail.com>
2021-09-25 20:41:37 -0300

    Fix AuthRequest

--- module-api/src/main/java/com/ivanicob/userservice/model/AuthRequest.java
+++ module-api/src/main/java/com/ivanicob/userservice/model/AuthRequest.java
@@ -1,5 +1,7 @@
 package com.ivanicob.userservice.model;
+import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.Length;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +11,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthRequest {
 
+	@NotNull(message = "Login is required.")
+	@Length(min=3, max=60, message="Login must contain between 3 and 60 characters.")
     private String login;
+	
+	@NotNull(message = "Password is required.")
+	@Length(min=6, message="Password must contain at least 6 characters.")
     private String password;
       
 }
